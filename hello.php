82cb7608c8a80c187b294717d4b5defce8b26cdd
tanvirtbf <tanver018765@gmail.com>
2024-07-02 14:40:53 +0600

    printf function declare

--- hello.php
+++ hello.php
@@ -17,10 +17,15 @@
   //constant variable
 
   //define const
-  define('BATCH','3');
-  define('SITE_NAME', 'http://tanvir.app');
+  // define('BATCH','3');
+  // define('SITE_NAME', 'http://tanvir.app');
   
   //Access const
-  echo BATCH;
-  echo SITE_NAME;
+  // echo BATCH;
+  // echo SITE_NAME;
+
+  //printf
+  $name = "Tanvir Ahmed";
+  $age = 25;
+  printf("Name : %s, Age : %d", $name,$age);
 ?>
