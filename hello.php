52dec443601ba9cd1e438abd5c6e181e94ddd911
tanvirtbf <tanver018765@gmail.com>
2024-07-03 12:28:16 +0600

    RGB color generate project complete with oop

--- hello.php
+++ hello.php
@@ -186,6 +186,35 @@
   // $ourFund->getTotalFund();
 
   // Project : Hexadecimal to decimal(RGB) 
+  class RGB{
+    private $color;
+    private $red;
+    private $green;
+    private $blue;
+
+    function __construct($colorCode=''){
+      $this->color = ltrim($colorCode,"#");
+    }
+    function getColor(){
+      return $this->color;
+    }
+    function setColor($colorCode){
+      $this->color = ltrim($colorCode,"#");
+      $this->parseColor();
+    }
+    private function parseColor(){
+      if($this->color){
+        list($this->red,$this->green,$this->blue) = sscanf($this->color,'%02x%02x%02x');
+        echo $this->red;
+        echo $this->blue;
+        echo $this->green;
+      }else{
+        list($this->red,$this->green,$this->blue) = array(0,0,0);
+      }
+    }
+  }
+  $myColor = new RGB("#ffef27");
+  $myColor->setColor("ffffff");
 
 
 
