1728d5980044a75f83af1c88bdd9f7fe7d5857cf
tanvirtbf <tanver018765@gmail.com>
2024-07-04 11:16:08 +0600

    Static Properties and Method

--- hello.php
+++ hello.php
@@ -331,41 +331,64 @@
   // $child=new Child();
 
   // Final keyword for inheritance
-  class Father{
-    public function __construct(){
-      echo "This is Father Constructor\n";
+  // class Father{
+  //   public function __construct(){
+  //     echo "This is Father Constructor\n";
+  //   }
+  //   public function print100($count=10){
+  //     for($i=0; $i<=100;$i+=$count){
+  //       echo "{$i}\n";
+  //     }
+  //   }
+  //   final public function print200($count=20){
+  //     for($i=0; $i<=200;$i+=$count){
+  //       echo "{$i}\n";
+  //     }
+  //   }
+  //   public function print300($count=30){
+  //     for($i=0; $i<=300;$i+=$count){
+  //       echo "{$i}\n";
+  //     }
+  //   }
+  // }
+  // class Child extends Father{
+  //   // public function demo($count){
+  //   //   parent::print100($count);
+  //   //   parent::print200($count);
+  //   //   parent::print300($count);
+  //   // }
+  //   final public function print200($count=20){
+  //     for($i=0; $i<=200;$i+=$count){
+  //       echo "{$i}\n";
+  //     }
+  //   }
+  // }
+  // $child=new Child();
+  // // $child->demo(33);
+  // $child->print200()
+
+  // Static Properties and Method
+
+  class FormValidation{
+    public function print1(){
+      echo "\nFirst Print\n";
     }
-    public function print100($count=10){
-      for($i=0; $i<=100;$i+=$count){
-        echo "{$i}\n";
-      }
+    public static function print2(){
+      echo "\nSecond Print\n";
     }
-    final public function print200($count=20){
-      for($i=0; $i<=200;$i+=$count){
-        echo "{$i}\n";
-      }
+    public function print3(){
+      echo "\nThird Print\n";
     }
-    public function print300($count=30){
-      for($i=0; $i<=300;$i+=$count){
-        echo "{$i}\n";
-      }
+    public static function print4(){
+      echo "\nFourth Print\n";
     }
   }
-  class Child extends Father{
-    // public function demo($count){
-    //   parent::print100($count);
-    //   parent::print200($count);
-    //   parent::print300($count);
-    // }
-    final public function print200($count=20){
-      for($i=0; $i<=200;$i+=$count){
-        echo "{$i}\n";
-      }
-    }
-  }
-  $child=new Child();
-  // $child->demo(33);
-  $child->print200()
+  FormValidation::print2();
+  FormValidation::print4();
+  $form = new FormValidation();
+  $form->print2();
+  $form->print1();
+  
 
 
 
