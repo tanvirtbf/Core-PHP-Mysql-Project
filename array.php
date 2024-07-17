8132cf7420c41048f4be961c67d295053e5cee2b
tanvirtbf <tanver018765@gmail.com>
2024-07-17 17:11:13 +0600

    array_slice and array_splice method

--- array.php
+++ array.php
@@ -99,7 +99,14 @@
 // array_unshift($unshiftArr,'Humaira','Khadija');
 // print_r($unshiftArr);
 
+//11. array_slice()
+$sliceArr = ['a','b','c','d','e'];
+$newSliceArr = array_slice($sliceArr,1,3);
+print_r($newSliceArr);
 
-
+//12. array_splice()
+$spliceArr = ['a','b','c','d','e'];
+array_splice($spliceArr,1,2,['tanvir','sadia','maymuna']);
+print_r($spliceArr);
 
 ?>
