480e039783a916f9af193650fbf24259b1510b4c
tanvirtbf <tanver018765@gmail.com>
2024-08-02 00:50:15 +0600

    Human class create and instant create

--- hello.php
+++ hello.php
@@ -121,6 +121,21 @@
   //     echo "My name is {$this->name}";
   //   }
   // }
+  Class Human{
+    public $name = "Tanvir Ahmed";
+    function sayHi(){
+      echo "Hello World";
+    }
+    function getName(){
+      echo "My name is {$this->name}";
+    }
+  }
+  $h1 = new Human();
+  echo $h1->name;
+  echo $h1->sayHi();
+  echo $h1->getName();
+
+
   // class Cat{
   //   function sayHi(){
   //     echo "Meow\n";
