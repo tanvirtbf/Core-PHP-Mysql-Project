f6b1373d66256d21ede78140d01b2ef966180dc9
tanvirtbf <tanver018765@gmail.com>
2024-08-02 00:31:12 +0600

    5 type of case in string

--- string.php
+++ string.php
@@ -9,5 +9,11 @@ echo strtolower($str); // hello world
 echo ucwords($str); // Hello World // Title case or Capitalized
 echo ucfirst($str); // Hello world // sentence er first word er first character uppercase hobe
 
+// various string case
+// 1. Title Case  = Hello World Tanvir
+// 2. snake case = hello_world_tanivr
+// 3. kebab case = hello-world-tanvir
+// 4. camel case = helloWorldTanvir
+// 5. pascel case = HelloWorldTanvir
  
 ?>
